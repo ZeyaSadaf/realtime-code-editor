b8837335da5f90b8e9fad4a43b3290d20c212ca8
ZeyaSadaf <zeyasadaf259@gmail.com>
2023-04-18 01:32:42 +0530

    updating component

--- server.js
+++ server.js
@@ -91,7 +91,7 @@ socket.on(ACTIONS.SYNC_CODE, ({socketId, code}) =>
 
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 server.listen(PORT, () =>
   console.log(`Server running on port ${PORT}`)
 );
